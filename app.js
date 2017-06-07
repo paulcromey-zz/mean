2619fc4ce18f5fd99dca7dd67aeef69979f482d9
Paul Cromey <paulcromey@hotmail.com>
2017-06-07 20:11:43 +0100

    Added npm start

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-require('./instantHello');
-var goodbye = require('./talk/goodbye');
-var talk = require('./talk');
-var question = require('./talk/question');
-
-talk.intro();
-talk.hello('Paul');
-
-var answer = question.ask("What is the meaning of life?");
-console.log(answer);
-
-goodbye();
+console.log("It works");
