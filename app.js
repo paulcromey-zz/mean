87525332644c59007b0120c0d716342d13580277
Paul Cromey <paulcromey@hotmail.com>
2017-06-30 12:26:13 +0100

    added static files

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const Console = require('console').Console;
+
+const logger = new Console(fs.createWriteStream('./stdout.log'));
 
 app.set('port', 3000);
 
-app.get('/', function(req, res) {
-	console.log('GET the homepage');
-	res.status(200).sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/json', function(req, res) {
-	console.log('GET the json');
-	res.status(200).json( {"jsonData" : true});
+	logger.time('INFO : method json');
+	res.status(200).json( {"jsonData" : false});
+	logger.timeEnd('INFO : method json');
 });
 
 app.get('/file', function(req, res) {
-	console.log('GET the file');
+	logger.time('INFO : method file');
+	const code = 200;
+	logger.error('ERROR :', code);
+	logger.info('INFO :', code);
 	res.status(200).sendFile(path.join(__dirname, 'app.js'));
+	logger.timeEnd('INFO : method file');
 });
 
-var server = app.listen(app.get('port'), function() {
+const server = app.listen(app.get('port'), function() {
 	var port = server.address().port;
-	console.log('Magic happens on port ' + port);
-});
+	console.info('Magic happens on port ' + port);
+});  
